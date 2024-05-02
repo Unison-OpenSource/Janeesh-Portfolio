def6fc64a4da4f7af215e2aeab0ac4388428319a
AKSHAYA R <22202008@rmd.ac.in>
2024-05-02 10:38:31 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
 import Profile from './me.png';
-import './Hero.css'
+import './Hero.css';
+
 const Hero = () => {
   return (
     <section className="hero-container">
-        <div className="hero-content">
-            <h2>The Future Belongs To Those Who Believe In The Beauty Of Their Dreams</h2>
-            <p>
-                FullStack Developer | AI  Enthusiast | Coder | Content Creator<br/>
-                
-                
-            </p>
-            <p>
-
-            </p>
-            </div>
-
-            <div className="hero-img">
-                <div>
-                <div className="tech-icon">
-                    <img src={Profile} width={400} height={400} alt="" />
-                </div>
-                </div>
-                
-            </div>
+      <div className="hero-content">
+        <h2>The Future Belongs To Those Who Believe In The Beauty Of Their Dreams</h2>
+        <p>
+          FullStack Developer | AI Enthusiast | Coder | Content Creator<br/>
+        </p>
+      </div>
+      
+      <div className="hero-img">
+        <div className="tech-icon">
+          <img src={Profile} width={400} height={400} alt="" />
+        </div>
+      </div>
     </section>
   )
 }
 
-export default Hero
+export default Hero;
+
