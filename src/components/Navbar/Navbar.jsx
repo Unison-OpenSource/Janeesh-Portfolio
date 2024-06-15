093c0edb62e7afd24863f1352d5f362997116313
my name <janeeshpofficial@gmail.com>
2024-06-15 23:42:59 +0530

    resume

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,50 +1,76 @@
-import React from 'react';
-import logo from './logo.png';
+import React from "react";
+import logo from "./logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
+  };
+  const openLink = () => {
+    window.location.href =
+      "https://drive.google.com/file/d/1gQ0TV9oWRbJV2_eYvNXwB8j1fO3H26O7/view?usp=drive_link";
   };
 
   return (
     <>
       <nav className="nav-wrapper">
         <div className="nav-content">
-          <img className="logo" src={logo} alt="JANEESH P" /> {/* Moved the logo after the menu */}
+          <img className="logo" src={logo} alt="JANEESH P" />{" "}
+          {/* Moved the logo after the menu */}
           <ul>
             <li>
-              <a className="menu-item" onClick={() => scrollToSection('home')}>Hero</a>
+              <a className="menu-item" onClick={() => scrollToSection("home")}>
+                Hero
+              </a>
             </li>
             <li>
-              <a className="menu-item" onClick={() => scrollToSection('skills')}>Skills</a>
+              <a
+                className="menu-item"
+                onClick={() => scrollToSection("skills")}
+              >
+                Skills
+              </a>
             </li>
             <li>
-              <a className="menu-item" onClick={() => scrollToSection('projects')}>Projects</a>
+              <a
+                className="menu-item"
+                onClick={() => scrollToSection("projects")}
+              >
+                Projects
+              </a>
             </li>
             <li>
-              <a className="menu-item" onClick={() => scrollToSection('experience')}>Experience</a>
+              <a
+                className="menu-item"
+                onClick={() => scrollToSection("experience")}
+              >
+                Experience
+              </a>
             </li>
             <li>
-              <a className="menu-item" onClick={() => scrollToSection('ac')}>Channel</a>
+              <a className="menu-item" onClick={() => scrollToSection("ac")}>
+                Channel
+              </a>
             </li>
             <li>
-              <a className="menu-item" onClick={() => scrollToSection('contact')}>Contact</a>
+              <a
+                className="menu-item"
+                onClick={() => scrollToSection("contact")}
+              >
+                Contact
+              </a>
             </li>
           </ul>
-
-
-          <button className="contact-btn" onClick={() => { alert('YOU CLICKED THE WRONG BUTTON') }}>
-            Magic
+          <button className="contact-btn" onClick={openLink}>
+            RESUME
           </button>
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
-
