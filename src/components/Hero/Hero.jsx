2eb36519568714c900f981b7075ef963bfb3bb38
janeeshgithub <janeeshthelegend@gmail.com>
2024-07-07 16:59:45 +0530

    abc

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import Profile from './me.png';
-import './Hero.css';
+import React from "react";
+import Profile from "./me.png";
+import "./Hero.css";
 
 const Hero = () => {
   return (
     <section className="hero-container">
       <div className="hero-content">
-        <h2>Under Maintenance</h2>
+        <h2>
+          Progress is impossible without change, and those who cannot change
+          their minds cannot change anything
+        </h2>
         <p>
-          FullStack Developer | AI Enthusiast | Coder | Content Creator<br/>
+          FullStack Developer | AI Enthusiast | Coder | Content Creator
+          <br />
         </p>
       </div>
-      
+
       <div className="hero-img">
         <div className="tech-icon">
           <img src={Profile} height={400} alt="" />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Hero;
-
