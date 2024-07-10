b0c378b54dd074f55263fa53a67c0a7635cbef35
janeeshgithub <janeeshthelegend@gmail.com>
2024-07-10 18:14:00 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/design" element={<Design />} />
     </Routes>
