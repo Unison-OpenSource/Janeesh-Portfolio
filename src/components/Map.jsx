5c94daacbcbcd3eb60db66a7c94d059b6862e867
janeeshgithub <janeeshthelegend@gmail.com>
2024-07-10 13:13:41 +0530

    jan

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const Map = () => {
+  const apiKey = 'AIzaSyDswIgnsvQxrHziqtTCKl-5HHoYlNFrcLE';
+  const mapContainerStyle = {
+    height: '400px',
+    width: '100%'
+  };
+  const center = {
+    lat: 0, // Center latitude
+    lng: 0  // Center longitude
+  };
+
+  const locations = [
+    { lat: 37.7749, lng: -122.4194 }, // Example location 1 (San Francisco)
+    { lat: 34.0522, lng: -118.2437 }, // Example location 2 (Los Angeles)
+    { lat: 40.7128, lng: -74.0060 }   // Example location 3 (New York)
+  ];
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <iframe
-        title="Google Map"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.6568467533184!2d77.66064029978071!3d12.993786338796141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1136218ea079%3A0xc6bf20ea89abad92!2sGoogle%20Bangalore%20-%20Old%20Madras%20Road!5e0!3m2!1sen!2sin!4v1720585502548!5m2!1sen!2sin"
-        width="400"
-        height="300"
-        style={{ border: 30 }}
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
-    </div>
+    <LoadScript googleMapsApiKey={apiKey}>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        center={center}
+        zoom={4}
+      >
+        {locations.map((location, index) => (
+          <Marker key={index} position={location} />
+        ))}
+      </GoogleMap>
+    </LoadScript>
   );
 };
 
