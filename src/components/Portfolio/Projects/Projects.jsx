5ebc2efb4e31bdab8dd742c4799321656b207d69
janeeshgithub <janeeshthelegend@gmail.com>
2024-07-14 00:18:06 +0530

    spI

--- src/components/Portfolio/Projects/Projects.jsx
+++ src/components/Portfolio/Projects/Projects.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import project from "./Projects.json";
 import "./Projects.css";
+import spidey from "./spid.jpg";
 const Projects = () => {
   return (
     <>
       <div className="text-center font-bold">
-        <h1 className="text-2xl md:text-4xl lg:text-5xl">Projects</h1><br></br>
+        <h1 className="text-2xl md:text-4xl lg:text-5xl">Projects</h1>
+        <br></br>
       </div>
       <div className="projects-container">
         {project.map((project, index) => (
           <div key={index} className="project-card">
-            <img src={project.imagesrc} alt={project.title} />
+            <img src={project.imagesrc || spidey} alt={project.title} />
             <h3>{project.title}</h3>
             <p>{project.description}</p>
             <p>Skills: {project.skills.join(", ")}</p>
