629fb3dc0f94c982d50bf6bc7edf8684b9c0eb7c
janeeshgithub <janeeshthelegend@gmail.com>
2024-07-16 18:51:10 +0530

    ok
    k

---
+++ src/Blog/Homeblog.jsx
@@ -0,0 +1,21 @@
+import { Typewriter } from "react-simple-typewriter";
+
+const Homeblog = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        <Typewriter
+          words={["Welcome to the Blog"]}
+          loop={1}
+          cursor
+          cursorStyle="_"
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
+      </h1>
+    </div>
+  );
+};
+
+export default Homeblog;
