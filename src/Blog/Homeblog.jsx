409861de3313acf59af0cde4252f4d5b903d5f32
janeeshgithub <janeeshthelegend@gmail.com>
2024-07-20 14:05:19 +0530

    Update Homeblog.jsx

--- src/Blog/Homeblog.jsx
+++ src/Blog/Homeblog.jsx
@@ -1,17 +1,27 @@
-import { Typewriter } from "react-simple-typewriter";
+import React from "react";
+import Typewriter from "react-typewriter-effect";
 
 const Homeblog = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 md:flex-row">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4 md:mb-0 md:mr-8">
+      <h1 className="text-4xl font-bold text-black mb-4 md:mb-0 md:mr-8">
         <Typewriter
-          words={["Welcome to the Blog KARUN"]}
-          loop={1}
-          cursor
-          cursorStyle="_"
-          typeSpeed={70}
-          deleteSpeed={50}
-          delaySpeed={1000}
+          textStyle={{
+            fontFamily: "Red Hat Display",
+            color: "#00000",
+            fontWeight: 500,
+            fontSize: "2em",
+          }}
+          startDelay={2000}
+          cursorColor="#00000"
+          multiText={[
+            "Welcome to the Blog",
+            "Janeesh P Welcomes You",
+            "I love You",
+          ]}
+          multiTextDelay={1000}
+          typeSpeed={30}
+          multiTextLoop
         />
       </h1>
     </div>
