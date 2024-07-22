b33cc2f896981ab5e287aa2e05335bad03356dc1
janeeshgithub <janeeshthelegend@gmail.com>
2024-07-22 11:53:11 +0530

    Update Homeblog.jsx

--- src/Blog/Homeblog.jsx
+++ src/Blog/Homeblog.jsx
@@ -3,21 +3,21 @@ import Typewriter from "react-typewriter-effect";
 
 const Homeblog = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-pink-100 md:flex-row">
-      <h1 className="text-4xl font-bold text-pink-800 mb-4 md:mb-0 md:mr-8">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-pink-100 px-4 md:flex-row">
+      <h1 className="text-3xl font-bold text-pink-800 mb-4 md:mb-0 md:mr-8 md:text-4xl">
         <Typewriter
           textStyle={{
             fontFamily: "'Comic Sans MS', cursive",
             color: "#D43F6C",
             fontWeight: 600,
-            fontSize: "2.5em",
+            fontSize: "1.5em", // Smaller font size for mobile
           }}
           startDelay={1000}
           cursorColor="#D43F6C"
           multiText={[
             "Welcome to the Blog",
             "Janeesh P Welcomes You",
-            "This site is under Construction",
+            "Hi da Macha",
           ]}
           multiTextDelay={1000}
           typeSpeed={60}
